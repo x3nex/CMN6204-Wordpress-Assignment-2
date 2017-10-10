4870b20d35e97b380e3f8d3ff112dac1d9da1cf6
x3nex <x3nex@hotmail.com>
2017-10-10 18:13:32 +0200

    DB exported, started work on addopting menu to the theme

--- wp-content/themes/portfolio/header.php
+++ wp-content/themes/portfolio/header.php
@@ -21,20 +21,15 @@
   <body>
 
 
-    <!-- Navigation -->
+   <!-- Navigation -->
     <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div class="container">
-        <a class="navbar-brand" href="index.html"><?= get_bloginfo('name') ?></a>
+        <a class="navbar-brand" href="index.php"><?= get_bloginfo('name') ?></a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
-            <li>
-              <?php wp_nav_menu(['theme_location' => 'main'])  ?>
-            </li>
-              
-         <!--    
             <li>
               <a href="about.html">Home</a>
             </li>
@@ -46,8 +41,13 @@
             </li>
             <li>
               <a href="about.html">Contact</a>
-            </li> -->
+            </li>
           </ul>
         </div>
       </div>
     </nav>
+
+
+<?php wp_nav_menu(['theme_location' => 'main'])  ?>
+
+  
